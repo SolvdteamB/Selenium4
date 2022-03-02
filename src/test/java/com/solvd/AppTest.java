d58ccb2c857540ea10744c54d0b57a6870aa223f
Martin Leg <solvdprojects@gmail.com>
2022-03-02 18:10:41 -0300

    first commit

---
+++ src/test/java/com/solvd/AppTest.java
@@ -0,0 +1,17 @@
+package com.solvd;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assert.assertTrue(true);
+    }
+}
---
+++ src/test/java/com/solvd/Chapter4.java
@@ -0,0 +1,59 @@
+package com.solvd;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Chapter4 {
+    WebDriver driver;
+    String url1 = "http://automationpractice.com/index.php";
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url1);
+        System.out.println("Title: " + driver.getTitle());
+    }
+
+    @Test
+    public void testNewWindowTab() {
+        WebDriver newWindow = driver.switchTo().newWindow(WindowType.WINDOW);
+        newWindow.get("http://automationpractice.com/index.php?controller=prices-drop");
+        System.out.println("Title: " + driver.getTitle());
+
+    }
+
+    @Test
+    public void testWorkingInBothWindowsTab() {
+
+        // TODO: Automatically Open and Switch to the new Windows
+        driver.switchTo().newWindow(WindowType.TAB)
+                .get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+        System.out.println("Title: " + driver.getTitle());
+
+        // TODO: Work in the new window or tab
+
+        driver.findElement(By.id("email_create")).sendKeys("Selenium4@TAU.com");
+        driver.findElement(By.id("SubmitCreate")).click();
+
+        // TODO: Get the window Id handels
+
+        // TODO: Switch & Workin the main window or tab
+
+    }
+
+    /*
+     * @AfterMethod
+     * public void tearDown() {
+     * driver.quit();
+     * }
+     */
+}
